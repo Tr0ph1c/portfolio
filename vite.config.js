932e15cca59649f59f1fbc72c550c91d9645d41c
Tr0ph1c <ahmmahmouda767@gmail.com>
2025-04-06 22:34:17 +0200

    gh-pages related fixes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/tr0ph1c.github.io",
+    base: "/portfolio",
 });
